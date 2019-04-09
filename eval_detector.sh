dd5809d1c3258269bfa70c022f1552aed34034da
gplhegde <gplhegde@gmail.com>
2019-04-09 16:56:03 +0800

    removing results from the repo

--- eval_detector.sh
+++ eval_detector.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 script=$(dirname $0)/pascalvoc.py
 python ${script} \
-    --gtfolder /opt/aimledge/datasets/person-detection/annotations/\
-    --detfolder /opt/aimledge/datasets/person-detection/voc-detections\
+    --gtfolder ${1}\
+    --detfolder ${2}\
     --threshold 0.5\
     -gtformat xyrb\
     --gt_file_format xml\
@@ -10,5 +10,5 @@ python ${script} \
     --det_file_format txt\
     -gtcoords abs\
     -detcoords abs\
-    --savepath ./det_eval_results\
+    --savepath ./results\
 
