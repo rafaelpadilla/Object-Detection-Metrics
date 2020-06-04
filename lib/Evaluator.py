56d56f2547c47d54913b08357b904aeb1c18aecb
johanmsk <31271632+johanmsk@users.noreply.github.com>
2020-06-04 21:04:48 +0200

    Recall is 1 if total positives is 0, not 'nan'
    If the class does not exist in the testing dataset then the recall is 1, not 'nan', since no positives exist to be missed.

--- lib/Evaluator.py
+++ lib/Evaluator.py
@@ -123,7 +123,10 @@ class Evaluator:
             # compute precision, recall and average precision
             acc_FP = np.cumsum(FP)
             acc_TP = np.cumsum(TP)
-            rec = acc_TP / npos
+            if npos != 0:
+                rec = acc_TP / npos
+            else:
+                rec = np.ones(len(dects))
             prec = np.divide(acc_TP, (acc_FP + acc_TP))
             # Depending on the method, call the right implementation
             if method == MethodAveragePrecision.EveryPointInterpolation:
