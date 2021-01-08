155c80cd901837c0ef13974159391e3b4c884dea
Yonghye Kwon <developer.0hye@gmail.com>
2021-01-08 22:55:33 +0900

    Sufficient way to measure FP
    FP is TP's reverse.
    We can get this from TP.
    I think that it's more efficient than original code.

--- lib/Evaluator.py
+++ lib/Evaluator.py
@@ -113,14 +113,17 @@ class Evaluator:
                         TP[d] = 1  # count as true positive
                         det[dects[d][0]][jmax] = 1  # flag as already 'seen'
                         # print("TP")
-                    else:
-                        FP[d] = 1  # count as false positive
+#                     else:
+#                         FP[d] = 1  # count as false positive
                         # print("FP")
                 # - A detected "cat" is overlaped with a GT "cat" with IOU >= IOUThreshold.
-                else:
-                    FP[d] = 1  # count as false positive
+#                 else:
+#                     FP[d] = 1  # count as false positive
                     # print("FP")
             # compute precision, recall and average precision
+            
+            FP = 1 - TP
+            
             acc_FP = np.cumsum(FP)
             acc_TP = np.cumsum(TP)
             rec = acc_TP / npos
